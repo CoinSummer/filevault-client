a790e7fa5c9aa97065b37ec327793238ea33c5e8
wenhui.yu <winfreyu@gmail.com>
2021-01-08 11:36:57 +0800

    fix: auth callback debug

--- src/pages/auth/[...from].tsx
+++ src/pages/auth/[...from].tsx
@@ -9,18 +9,17 @@ const AuthPage = () => {
   const router = useRouter()
   const dispatch = useDispatch<AppDispatch>()
 
-  useEffect(() => {
-    const { from } = router.query
-    console.log('from: ', from)
-    if (from) {
-      console.log('get tweets list')
-      if (from[0] === 'twitter') {
-        const { oauth_token: oauthToken, oauth_verifier: oauthVerifier } = router.query
-        console.log('oauthToken', oauthToken, 'oauthVerifier', oauthVerifier)
-        dispatch(getTwitterCallback())
-      }
+  const { from } = router.query
+  console.log('router.query: ', router.query);
+  console.log('from: ', from)
+  if (from) {
+    console.log('get tweets list')
+    if (from[0] === 'twitter') {
+      const { oauth_token: oauthToken, oauth_verifier: oauthVerifier } = router.query
+      console.log('oauthToken', oauthToken, 'oauthVerifier', oauthVerifier)
+      dispatch(getTwitterCallback())
     }
-  }, [dispatch])
+  }
 
   return (
     <BaseLayout>

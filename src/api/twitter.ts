43e6dc81fdfb73be3e031ac76af8caace95c2837
wenhui.yu <winfreyu@gmail.com>
2021-01-11 22:37:32 +0800

    fix: twitter oauth refactor

--- src/api/twitter.ts
+++ src/api/twitter.ts
@@ -2,7 +2,7 @@ import request from '../utils/request'
 
 export const fetchTweetsList = (params?: any): Promise<any> => {
   return request({
-    url: `/twitter`,
+    url: `/twitter/tweets`,
     method: 'GET',
     params,
   })
